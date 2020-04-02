8c003538057d28d23598d14fa6ebc37ae44e1bea
Dylan Ambrose <djambro44@gmail.com>
2020-04-02 15:04:04 -0700

    linked projects to carasuel and added brief description

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 $("document").ready(function() {
 
     $("#nameButton").on("click", function(){
+        // $("#myBar").move();
         $("#carouselExampleCaptions").hide();
         $("#contactForm").hide();
         $("#aboutMeContent").hide();
@@ -28,6 +29,26 @@ $("document").ready(function() {
         $("#contactForm").hide();
         $("#aboutMeContent").show();
     })
+
+//         var i = 0;
+// function move() {
+//   if (i == 0) {
+//     i = 1;
+//     var elem = document.getElementById("myBar");
+//     var width = 1;
+//     var id = setInterval(frame, 10);
+//     function frame() {
+//       if (width >= 100) {
+//         clearInterval(id);
+//         i = 0;
+//       } else {
+//         width++;
+//         elem.style.width = width + "%";
+//       }
+//     }
+//   }
+// }
+    
 
 
 
